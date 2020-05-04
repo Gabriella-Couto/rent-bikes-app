f82f8f2ebd01be79f420baa5cb5b93e0ff29e771
Luiza Magalhaes <luizalmagalhaes@hotmail.com>
2020-05-04 18:31:19 -0300

    Começando a estilizar as telas existentes

--- Componentes/VerBicicleta.js
+++ Componentes/VerBicicleta.js
@@ -1,62 +1,96 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, Image, ScrollView } from 'react-native';
-import {withNavigation} from 'react-navigation';
+import { StyleSheet, Text, View, Button, Image, ScrollView, TouchableOpacity } from 'react-native';
+import { withNavigation } from 'react-navigation';
+import colors from "../colors";
 
 const VerBicicleta = ({ navigation }) => {
-    const alugarClick = () => {
+  const alugarClick = () => {
+  }
 
-    }
+  return (
+    <View style={styles.container}>
+      <ScrollView>
+        <View style={{ alignItems: 'center', marginHorizontal: 30 }}>
+          <Image style={styles.productImg} source={require('../Imagens/1.jpg')} />
+          <Text style={styles.name}>Bicicleta de passeio</Text>
+          <Text style={styles.price}>R$ 5,00/h</Text>
+          <Text style={styles.description}>
+            Esse conjunto de componentes é indicado para passeios em qualquer tipo de terreno com total conforto e segurança sem perder o estilo.
+            </Text>
+        </View>
+        <View style={styles.separator}></View>
+        <TouchableOpacity
+          style={styles.btn}
+          mode="contained"
+          onPress={() => { navigation.navigate('Pagamento') }}
+        >
+          <Text style={styles.btnText}>Alugar</Text>
+        </TouchableOpacity>
 
-    return (
-        <ScrollView >
-            <View style={styles.container}>
-                <Image style={styles.imagem} source={require('../Imagens/1.jpg')}/>
-                <Text style={styles.title}>Bicicleta 29 KSW 24v Câmbios Shimano Tx-800 Disco Hidráulico Suspensão com Trava Branco e Preto 17</Text>
-                <View >
-                <Text style={styles.lines}>Preço por hora: R$ 5,00</Text>
-                <Text style={styles.lines}>Preço por dia: R$ 100,00</Text>
-                <Text style={styles.lines}>Detalhes: A Bicicleta Aro 29 KSW Xlt 24V Câmbios Shimano Freio a Disco Hidráulico Suspensão com Trava Quadro em Alumínio 6061 é equipada com Câmbios Shimano.
-                O freio a disco é desenvolvido para melhor desempenho nas frenagens em qualquer terreno, quadro em alumínio 6061 e suspensão dianteira com 80mm de Curso.
-                Esse conjunto de componentes é indicado para passeios em qualquer tipo de terreno com total conforto e segurança sem perder o estilo.</Text>
-                <Text style={styles.lines}>Anunciado por: Edivaldo</Text>
-                <Text style={styles.lines}>Retirada em: Butantã, São Paulo - SP</Text>
-                </View>
-                <View style={styles.btn1}>
-                    <Button title="Alugar" onPress={() => {navigation.navigate('Pagamento')}}/>
-                </View>
-            </View>
-           
-        </ScrollView>
-    );
+      </ScrollView>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    marginTop: 20,
+    alignItems: 'center',
     backgroundColor: '#fff',
-    margin: 15
+    margin: 15,
+    height: 200
   },
-  title: {
-    fontSize: 16,
+  productImg: {
+    width: 200,
+    height: 200,
+  },
+  name: {
+    fontSize: 28,
+    color: "#696969",
+    fontWeight: 'bold'
+  },
+  price: {
+    marginTop: 10,
+    fontSize: 18,
+    color: "green",
+    fontWeight: 'bold'
+  },
+  description: {
+    textAlign: 'center',
+    marginTop: 10,
+    color: "#696969",
+  },
+  star: {
+    width: 40,
+    height: 40,
+  },
+
+  separator: {
+    height: 2,
+    backgroundColor: "#eeeeee",
+    marginTop: 20,
+    marginHorizontal: 30
+  },
+
+  btn: {
+    marginVertical: 30,
+    backgroundColor: colors.venus400,
+    padding: 5,
+    color: colors.moon1000,
+    borderRadius: 30,
+    width: 125,
+    alignSelf: 'center',
+
+  },
+  btnText: {
+    fontSize: 20,
+    padding: 7,
+    color: "white",
+    textAlign: "center",
     fontWeight: 'bold',
-    marginBottom: 15
-  },
-  imagem: {
-      width: 300,
-      height: 300
-  },
-  btn1: {
-    width: 100,
-    margin: 20
-  },
-  lines:{
-    marginBottom: 5
-  }, price: {
-    fontWeight: 'bold',
-    marginBottom: 5,
-    color: '#32adf0'
+    alignSelf: "center"
   }
 
 });
-
 export default withNavigation(VerBicicleta)
--- Navigation/Index.js
+++ Navigation/Index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createStackNavigator } from 'react-navigation-stack'
 import { createAppContainer } from 'react-navigation'
 import TelaInicio from '../Componentes/TelaInicio';
-import VerBicicleta  from '../Componentes/VerBicicleta';
+import VerBicicleta from '../Componentes/VerBicicleta';
 import Pagar from '../Componentes/Pagamentos/Pagar';
 import { Platform } from 'react-native';
 
@@ -11,10 +11,10 @@ const Index = createStackNavigator({
     TelaInicio: TelaInicio,
     VerBicicleta: VerBicicleta,
     Pagamento: Pagar
-},{
-    defaultNavigationOptions: {
-        headerTintColor: Platform.OS === 'android' ? '#9d1cba' : '#9d1cba'
-    }
-})
+}, {
+        defaultNavigationOptions: {
+            headerTintColor: Platform.OS === 'android' ? '#842a6c' : '#842a6c'
+        }
+    })
 
 export default createAppContainer(Index);
